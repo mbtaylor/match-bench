cae8afcc2f541b15dba64961fe408149e1fa7986
Mark Taylor <m.b.taylor@bristol.ac.uk>
2021-09-02 18:28:26 +0100

    some changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 usage="
-   Usage: $0 -n <nrow>
+   Usage: $0 -n <nrow> -stilts <stilts-cmd>
 "
 
 stilts="stilts"
@@ -18,6 +18,11 @@ do
           shift
           shift
           ;;
+      -stilts)
+          stilts="$2"
+          shift
+          shift
+          ;;
       -h|-help|--help)
           echo "$usage"
           exit 0
@@ -31,19 +36,26 @@ done
 
 make build
 
+t0=t0-$nrow.fits
 t1=t1-$nrow.fits
 t2=t2-$nrow.fits
 
-if [ ! -f $t1 ]
+if [ ! -f $t0 -o ! -f $t1 -o ! -f $t2 ]
 then
-   $stilts tpipe in=:skysim:$nrow cmd=progress out=$t1
-   echo $t1
-fi
+   rm -f $t0 $t1 $t2
 
-if [ ! -f $t2 ]
-then
+   $stilts tpipe in=:skysim:$nrow cmd=progress out=$t0
+   echo $t0
+
+   $stilts tpipe in=$t0 \
+           cmd=progress \
+           cmd='select $0%11>=2' \
+           out=$t1
+   echo $t1
+   $stilts tpipe in=$t1 omode=count
+
    $stilts -Djel.classes=SkyLib -classpath . \
-           tpipe in=$t1 \
+           tpipe in=$t0 \
            cmd=progress \
            cmd='addcol -units deg ra0 ra' \
            cmd='addcol -units deg dec0 dec' \
@@ -51,16 +63,20 @@ then
            cmd='addcol -units deg -ucd "pos.eq.ra;meta.main" ra1 pos1[0]' \
            cmd='addcol -units deg -ucd "pos.eq.dec;meta.main" dec1 pos1[1]' \
            cmd='keepcols "ra1 dec1 ra0 dec0"' \
-           cmd='select $0/10>=2' \
+           cmd='select $0%10>=2' \
            out=$t2
    echo $t2
+   $stilts tpipe in=$t2 omode=count
 fi
 
-$stilts -bench \
+cmd="$stilts -J-ea -bench \
         tmatch2 progress=log \
                 matcher=sky \
                 in1=$t1 values1='ra dec' \
                 in2=$t2 values2='ra1 dec1' \
                 params="$maxarcsec" \
-                omode=count 
+                find=best \
+                omode=checksum"
+echo $cmd
+eval $cmd
 
